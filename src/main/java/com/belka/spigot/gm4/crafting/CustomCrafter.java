9e7878a95a6c546d7c6df1e20573cd4737935905
maximumfx <Efteling342>
2019-03-12 16:08:19 +0100

    Started CustomCrafter

--- src/main/java/com/belka/spigot/gm4/crafting/CustomCrafter.java
+++ src/main/java/com/belka/spigot/gm4/crafting/CustomCrafter.java
@@ -1,5 +1,8 @@
 package com.belka.spigot.gm4.crafting;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
@@ -8,7 +11,13 @@ public class CustomCrafter implements Listener {
 	
 	@EventHandler
     public void onItemDrop(PlayerDropItemEvent e) {
-		//TODO Empty stub
+		Item i = e.getItemDrop();
+		Location loc = i.getLocation();
+		if (loc.add(0,-1,0).getBlock().getBlockData().getMaterial().equals(Material.DROPPER)) {
+			if (i.getItemStack().getData().getItemType().equals(Material.CRAFTING_TABLE)) {
+
+			}
+		}
 	}
 
 }
