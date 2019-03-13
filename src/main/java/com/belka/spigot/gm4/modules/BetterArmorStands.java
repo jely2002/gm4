c3869f2e9c850301b2a721af8ea4efdeb177e23c
MaximumFX <draconiconraveleijn@gmail.com>
2019-03-13 15:17:30 +0100

    Updating damaging shears

--- src/main/java/com/belka/spigot/gm4/modules/BetterArmorStands.java
+++ src/main/java/com/belka/spigot/gm4/modules/BetterArmorStands.java
@@ -5,11 +5,13 @@ import com.belka.spigot.gm4.MainClass;
 import com.belka.spigot.gm4.interfaces.Initializable;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class BetterArmorStands implements Initializable {
 
@@ -22,23 +24,28 @@ public class BetterArmorStands implements Initializable {
 	public void init(MainClass mc) {
 		mc.getServer().getScheduler().scheduleSyncRepeatingTask(mc, () -> {
 			if(mc.getConfig().getBoolean("modules.BetterArmorStands.enabled")) {
-				for(Entity entity : Bukkit.getWorld("world").getEntities()) {
-					if(entity instanceof Item) {
-					    ItemStack item = ((Item) entity).getItemStack();
-						if(entity.isOnGround()) {
-							for(Entity e : Helper.getNearbyEntities(entity.getLocation(), 1)) {
-								if(e instanceof ArmorStand) {
-									Material itemMat = ((Item) entity).getItemStack().getType();
-									ArmorStand as = (ArmorStand) e;
-									if(itemMat == Material.STICK && item.getAmount() >= 2 && !as.hasArms()) {
-										as.setArms(true);
-										for(int i = 0; i < 2; i++) {
-											entity.remove();
+				for(World world : Bukkit.getWorlds()) {
+					for(Entity entity : world.getEntities()) {
+						if (entity instanceof Item) {
+							ItemStack item = ((Item) entity).getItemStack();
+							if (entity.isOnGround()) {
+								for (Entity e : Helper.getNearbyEntities(entity.getLocation(), 1)) {
+									if (e instanceof ArmorStand) {
+										Material itemMat = ((Item) entity).getItemStack().getType();
+										ArmorStand as = (ArmorStand) e;
+										if (itemMat == Material.STICK && item.getAmount() >= 2 && !as.hasArms()) {
+											as.setArms(true);
+											for (int i = 0; i < 2; i++) {
+												entity.remove();
+											}
 										}
-									}
-									if(itemMat == Material.SHEARS && !as.isSmall()) {
-										as.setSmall(true);
-                                        Damageable itemMeta = (Damageable) item.getItemMeta();
+										if (itemMat == Material.SHEARS && !as.isSmall()) {
+											as.setSmall(true);
+											ItemStack shears = new ItemStack(Material.SHEARS);
+											Damageable shearsMeta = (Damageable) shears.getItemMeta();
+											shearsMeta.setDamage(1);
+											item.setItemMeta((ItemMeta) shearsMeta);
+										}
 									}
 								}
 							}
