562320398a67be5e83ac0674d052e8aa73b6c082
maximumfx <Efteling342>
2019-03-13 09:26:15 +0100

    Shortened code

--- src/main/java/com/belka/spigot/gm4/modules/DesireLines.java
+++ src/main/java/com/belka/spigot/gm4/modules/DesireLines.java
@@ -42,13 +42,9 @@ public class DesireLines implements Listener {
 					if(replace == Material.AIR) {
 						below = p.getLocation().subtract(0.0, 1.0, 0.0).getBlock();
 					}
-//					if(below.getType() == Material.GRASS_BLOCK || below.getType() == Material.DIRT || below.getType() == Material.SAND) {
+
 					below.getWorld().spawnParticle(Particle.BLOCK_CRACK, below.getLocation().add(0.5, 0.5, 0.5), 10, below.getType());
-//					}
-//					if(replace == Material.TALL_GRASS || replace == Material.LEGACY_DOUBLE_PLANT || replace == Material.BROWN_MUSHROOM || replace == Material.RED_MUSHROOM || replace == Material.LEGACY_YELLOW_FLOWER || replace == Material.DEAD_BUSH) {
-//						below.getWorld().spigot().playEffect(below.getLocation().add(0.5, 0.5, 0.5), Effect.TILE_BREAK, below, (int)below.getData(), 0.0f, 0.0f, 0.0f, 0.0f, 10, 16);
-//						below.getWorld().dropItem(below.getLocation(), replace);
-//					}
+
 					replace = replacement(below.getType());
 					below.setType(replace);
 					below.getState().update();
@@ -65,43 +61,13 @@ public class DesireLines implements Listener {
 				return Material.COARSE_DIRT;
 			case SAND:
 				return Material.SANDSTONE;
-			case TALL_GRASS:
-				return Material.AIR;
-			case BROWN_MUSHROOM:
-				return Material.AIR;
-			case RED_MUSHROOM:
-				return Material.AIR;
-			case DEAD_BUSH:
-				return Material.AIR;
-			case DANDELION:
-				return Material.AIR;
-			case POPPY:
-				return Material.AIR;
-			case BLUE_ORCHID:
-				return Material.AIR;
-			case ALLIUM:
-				return Material.AIR;
-			case AZURE_BLUET:
-				return Material.AIR;
-			case ORANGE_TULIP:
-				return Material.AIR;
-			case PINK_TULIP:
-				return Material.AIR;
-			case RED_TULIP:
-				return Material.AIR;
-			case WHITE_TULIP:
-				return Material.AIR;
-			case OXEYE_DAISY:
-				return Material.AIR;
-			case SUNFLOWER:
-				return Material.AIR;
-			case LILAC:
-				return Material.AIR;
-			case ROSE_BUSH:
-				return Material.AIR;
-			case PEONY:
-				return Material.AIR;
 			default:
+				if(i == Material.BROWN_MUSHROOM || i == Material.RED_MUSHROOM || i == Material.DEAD_BUSH || i == Material.GRASS || i == Material.FERN ||
+						i == Material.DANDELION || i == Material.POPPY || i == Material.BLUE_ORCHID || i == Material.ALLIUM || i == Material.AZURE_BLUET || i == Material.OXEYE_DAISY ||
+						i == Material.ORANGE_TULIP || i == Material.PINK_TULIP || i == Material.RED_TULIP || i == Material.WHITE_TULIP ||
+						i == Material.SUNFLOWER || i == Material.LILAC || i == Material.ROSE_BUSH || i == Material.PEONY || i == Material.TALL_GRASS || i == Material.LARGE_FERN) {
+					return Material.AIR;
+				}
 				return i;
 		}
 	}
