30c10aae36be04364f1f8a2a1cccd7ae15f709fc
MaximumFX <draconiconraveleijn@gmail.com>
2019-03-15 07:52:48 +0100

    Working on Advancements

--- src/main/java/com/belka/spigot/gm4/modules/Advancements.java
+++ src/main/java/com/belka/spigot/gm4/modules/Advancements.java
@@ -1,6 +1,7 @@
 package com.belka.spigot.gm4.modules;
 
 import com.belka.spigot.gm4.MainClass;
+import com.belka.spigot.gm4.crafting.CustomItems;
 import com.belka.spigot.gm4.interfaces.Initializable;
 import eu.endercentral.crazy_advancements.*;
 import eu.endercentral.crazy_advancements.manager.AdvancementManager;
@@ -12,6 +13,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class Advancements implements Listener, Initializable {
 	
@@ -34,17 +36,22 @@ public class Advancements implements Listener, Initializable {
 		manager.addAdvancement(gm4);
 
 		Advancement cc = getParentAdvancement(gm4, "clever_crafting", "Clever Crafting", "Build yourself a Custom Crafter with droppers and a crafting table.", Material.CRAFTING_TABLE, AdvancementFrame.CHALLENGE, true, true, AdvancementVisibility.ALWAYS, 1f, 0f);
-		addAdvancement(cc, "clever_decrafting", "Clever Decrafting", "Assemble a Disassembler.", Material.DROPPER, AdvancementFrame.TASK, true, true, AdvancementVisibility.PARENT_GRANTED, 2f, -1.5f);
-		addAdvancement(cc, "clever_smelting", "Clever Smelting", "Create your own Blast Furnace.", Material.FURNACE, AdvancementFrame.TASK, true, true, AdvancementVisibility.PARENT_GRANTED, 2f, -0.5f);
-		addAdvancement(cc, "clever_crushing", "Clever Crushing", "Make yourself a new Block Compressor using a Custom Crafter.", Material.PISTON, AdvancementFrame.TASK, true, true, AdvancementVisibility.PARENT_GRANTED, 2f, 0.5f);
-		addAdvancement(cc, "green_feet", "Green Feet", "Wear the Boots of Ostara.", Material.LEATHER_BOOTS, AdvancementFrame.TASK, true, true, AdvancementVisibility.PARENT_GRANTED, 2.5f, 1.5f);
+		addAdvancement(cc, "clever_decrafting", "Clever Decrafting", "Assemble a Disassembler.", Material.DROPPER, AdvancementFrame.TASK, true, true, AdvancementVisibility.PARENT_GRANTED, 2f, -1f);
+		addAdvancement(cc, "clever_smelting", "Clever Smelting", "Create your own Blast Furnace.", Material.FURNACE, AdvancementFrame.TASK, true, true, AdvancementVisibility.PARENT_GRANTED, 2f, 0f);
+		addAdvancement(cc, "clever_crushing", "Clever Crushing", "Make yourself a new Block Compressor using a Custom Crafter.", Material.PISTON, AdvancementFrame.TASK, true, true, AdvancementVisibility.PARENT_GRANTED, 2f, 1f);
+		addAdvancement(cc, "green_feet", "Green Feet", "Wear the Boots of Ostara.", Material.LEATHER_BOOTS, AdvancementFrame.TASK, true, true, AdvancementVisibility.PARENT_GRANTED, 2f, 2f);
 
-		Advancement hc = getParentAdvancement(gm4, "clever_crafting", "Clever Crafting", "Build yourself a Custom Crafter with droppers and a crafting table.", Material.CRAFTING_TABLE, AdvancementFrame.CHALLENGE, true, true, AdvancementVisibility.ALWAYS, 1f, 0f);
+		Advancement hc = getParentAdvancement(cc, "cant_buy_me_love", "Can't buy me love", "Craft a Tier 1 Heart Canister.", CustomItems.HEART_CANISTER_TIER_1(1), AdvancementFrame.GOAL, true, true, AdvancementVisibility.PARENT_GRANTED, 2f, -2f);
+		Advancement hc2 = getParentAdvancement(hc, "love_is_all_you_need", "Love is all you need", "Craft a Tier 2 Heart Canister.", CustomItems.HEART_CANISTER_TIER_2(1), AdvancementFrame.GOAL, true, true, AdvancementVisibility.PARENT_GRANTED, 3f, -2f);
+		addAdvancement(hc2, "all_my_loving", "All My Loving.", "Max out your Heart Canister health.", CustomItems.HEART_CANISTER_TIER_2(1), AdvancementFrame.TASK, true, true, AdvancementVisibility.PARENT_GRANTED, 4f, -2f);
 
-		Advancement mobs = getParentAdvancement(cc, "mobs", "Mobs", "", Material.IRON_SWORD, AdvancementFrame.TASK, false, false, AdvancementVisibility.ALWAYS, 1f, 3f);
-		addAdvancement(gm4, "natural_defences", "Natural Defenses", "Get blinded by an underwater squid.", Material.INK_SAC, AdvancementFrame.TASK, true, true, AdvancementVisibility.ALWAYS, 2f, 2f);
-		addAdvancement(gm4, "ender_aid", "Ender Aid", "Come in contact with a support Enderman.", Material.ENDER_EYE, AdvancementFrame.TASK, true, true, AdvancementVisibility.ALWAYS, 2f, 3f);
-		addAdvancement(gm4, "risen", "Risen", "Die and raise up an undead zombie in your body's place.", Material.TOTEM_OF_UNDYING, AdvancementFrame.TASK, true, true, AdvancementVisibility.ALWAYS, 2f, 4f);
+		Advancement mobs = getParentAdvancement(gm4, "mobs", "Mobs", "", Material.IRON_SWORD, AdvancementFrame.TASK, false, false, AdvancementVisibility.ALWAYS, 2f, 3f);
+		addAdvancement(mobs, "natural_defences", "Natural Defenses", "Get blinded by an underwater squid.", Material.INK_SAC, AdvancementFrame.TASK, true, true, AdvancementVisibility.ALWAYS, 3f, 0.5f);
+		addAdvancement(mobs, "ender_aid", "Ender Aid", "Come in contact with a support Enderman.", Material.ENDER_EYE, AdvancementFrame.TASK, true, true, AdvancementVisibility.ALWAYS, 3f, 1.5f);
+		addAdvancement(mobs, "risen", "Risen", "Die and raise up an undead zombie in your body's place.", Material.TOTEM_OF_UNDYING, AdvancementFrame.TASK, true, true, AdvancementVisibility.ALWAYS, 3f, 2.5f);
+		addAdvancement(mobs, "batboozled", "Batboozled", "Get blown up by a Bat Grenade.", Material.TNT, AdvancementFrame.TASK, true, true, AdvancementVisibility.ALWAYS, 3f, 3.5f);
+		addAdvancement(mobs, "dr_frankenstein", "Dr. Frankenstein", "It's alive! Create a Shulker.", Material.SHULKER_BOX, AdvancementFrame.TASK, true, true, AdvancementVisibility.ALWAYS, 3f, 4.5f);
+		addAdvancement(mobs, "instant_regret", "Instant Regret", "Wither-ize a skeleton with potion swords.", Material.WITHER_SKELETON_SKULL, AdvancementFrame.TASK, true, true, AdvancementVisibility.ALWAYS, 3f, 5.5f);
 
 //		addAdvancement(gm4, "all_my_audreys", "All my Audreys", "Find all Dearest Audrey messages in a bottle.", Material.GLASS_BOTTLE, AdvancementFrame.TASK, true, true, AdvancementVisibility.ALWAYS, 1f, -1f);
 //		addAdvancement(gm4, "a_fun_gi", "A Fun-gi", "Decor some decorative mushroom.", Material.RED_MUSHROOM_BLOCK, AdvancementFrame.TASK, true, true, AdvancementVisibility.ALWAYS, 1f, -2f);
@@ -58,35 +65,6 @@ public class Advancements implements Listener, Initializable {
 
 		// OLD
 //		// HEART CANISTER
-//		AdvancementAPI hc = AdvancementAPI.builder(new NamespacedKey(mc, "gm4/cant_buy_me_love"))
-//			.title("Can't buy me love")
-//			.description("Craft a Tier 1 Heart Canister.")
-//			.icon("minecraft:dragon_breath")
-//			.trigger(Trigger.builder(Trigger.TriggerType.IMPOSSIBLE, "default"))
-//			.announce(true)
-//			.hidden(false)
-//			.toast(true)
-//			.background(gm4.getBackground())
-//			.frame(FrameType.TASK)
-//    		.parent(gm4.getId().toString())
-//			.build();
-//
-//		hc.save(worldName);
-//
-//		AdvancementAPI hc2 = AdvancementAPI.builder(new NamespacedKey(mc, "gm4/love_is_all_you_need"))
-//			.title("Love is all you need")
-//			.description("Craft a Tier 2 Heart Canister.")
-//			.icon("minecraft:dragon_breath")
-//			.trigger(Trigger.builder(Trigger.TriggerType.IMPOSSIBLE, "default"))
-//			.announce(true)
-//			.hidden(false)
-//			.toast(true)
-//			.background(gm4.getBackground())
-//			.frame(FrameType.TASK)
-//	    	.parent(hc.getId().toString())
-//			.build();
-//
-//		hc2.save(worldName);
 //
 //		addAdvancementToParent("gm4/all_my_loving", "All My Loving.", "Max out your Heart Canister health", "minecraft:dragon_breath",
 //				Trigger.builder(Trigger.TriggerType.IMPOSSIBLE, "default"), true, false, true, FrameType.TASK, hc2, worldName);
@@ -106,21 +84,6 @@ public class Advancements implements Listener, Initializable {
 //			.build();
 //
 //		mobs.save(worldName);
-//
-//		addAdvancementToParent("gm4/natural_defences", "Natural Defences", "Get blinded by an underwater squid.", "minecraft:ink_sac",
-//				Trigger.builder(Trigger.TriggerType.IMPOSSIBLE, "default"), true, false, true, FrameType.TASK, mobs, worldName);
-//
-//		addAdvancementToParent("gm4/ender_aid", "Ender Aid", "Come in contact with a support Enderman.", "minecraft:ender_pearl",
-//				Trigger.builder(Trigger.TriggerType.IMPOSSIBLE, "default"), true, false, true, FrameType.TASK, mobs, worldName);
-//
-//		addAdvancementToParent("gm4/batboozled", "Batboozled", "Get blown up by a Bat Grenade. XD", "minecraft:tnt",
-//				Trigger.builder(Trigger.TriggerType.IMPOSSIBLE, "default"), true, false, true, FrameType.TASK, mobs, worldName);
-//
-//		addAdvancementToParent("gm4/dr_frankenstein", "Dr. Frankenstein", "It's alive! Create a Shulker.", "minecraft:purple_shulker_box",
-//				Trigger.builder(Trigger.TriggerType.IMPOSSIBLE, "default"), true, false, true, FrameType.CHALLENGE, mobs, worldName);
-//
-//		addAdvancementToParent("gm4/instant_regret", "Instant Regret", "Wither-ize a skeleton with potion swords.", "minecraft:coal",
-//				Trigger.builder(Trigger.TriggerType.IMPOSSIBLE, "default"), true, false, true, FrameType.CHALLENGE, mobs, worldName);
 //
 //		// ARMOR STANDS
 //		AdvancementAPI ar = AdvancementAPI.builder(new NamespacedKey(mc, "gm4/gettin_handsy"))
@@ -150,8 +113,21 @@ public class Advancements implements Listener, Initializable {
 		Advancement child = new Advancement(parent, new NameKey("gm4", key), display);
 		manager.addAdvancement(child);
 	}
+	public void addAdvancement(Advancement parent, String key, String title, String description, ItemStack icon, AdvancementFrame frame, boolean showToast, boolean announceChat, AdvancementVisibility visibility, float x, float y) {
+		AdvancementDisplay display = new AdvancementDisplay(icon, title, description, frame, showToast, announceChat, visibility);
+		display.setCoordinates(x, y);
+		Advancement child = new Advancement(parent, new NameKey("gm4", key), display);
+		manager.addAdvancement(child);
+	}
 
 	public Advancement getParentAdvancement(Advancement parent, String key, String title, String description, Material icon, AdvancementFrame frame, boolean showToast, boolean announceChat, AdvancementVisibility visibility, float x, float y) {
+		AdvancementDisplay display = new AdvancementDisplay(icon, title, description, frame, showToast, announceChat, visibility);
+		display.setCoordinates(x, y);
+		Advancement child = new Advancement(parent, new NameKey("gm4", key), display);
+		manager.addAdvancement(child);
+		return child;
+	}
+	public Advancement getParentAdvancement(Advancement parent, String key, String title, String description, ItemStack icon, AdvancementFrame frame, boolean showToast, boolean announceChat, AdvancementVisibility visibility, float x, float y) {
 		AdvancementDisplay display = new AdvancementDisplay(icon, title, description, frame, showToast, announceChat, visibility);
 		display.setCoordinates(x, y);
 		Advancement child = new Advancement(parent, new NameKey("gm4", key), display);
@@ -161,10 +137,10 @@ public class Advancements implements Listener, Initializable {
 
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent e) {
-		manager.addPlayer(e.getPlayer());
-		manager.loadProgress(e.getPlayer(), "gm4");
-		grantAdvancement("root", e.getPlayer());
-		Bukkit.broadcastMessage("Welcome " + e.getPlayer().getName());
+		Player p = e.getPlayer();
+		manager.addPlayer(p);
+		manager.loadProgress(p, "gm4");
+		grantAdvancement("root", p);
 	}
 
 	@EventHandler
