d7d9bbcefb33c63435540aabdc74109729ff5f5c
MaximumFX <draconiconraveleijn@gmail.com>
2019-03-16 10:17:10 +0100

    Updated BatGrenades

--- src/main/java/com/belka/spigot/gm4/modules/BatGrenades.java
+++ src/main/java/com/belka/spigot/gm4/modules/BatGrenades.java
@@ -20,32 +20,31 @@ public class BatGrenades implements Initializable {
 	}
 
 	public void init(MainClass mc) {
+		if(!mc.getConfig().getBoolean("BatGrenades.enabled")) return;
 		mc.getServer().getScheduler().scheduleSyncRepeatingTask(mc, () -> {
-			if(mc.getConfig().getBoolean("modules.BatGrenades.enabled")) {
 				for(World w : Bukkit.getWorlds()) {
-					for(Entity entity : w.getEntities()) {
-						if(entity instanceof Player) {
+					for (Entity entity : w.getEntities()) {
+						if (entity instanceof Player) {
 							Player p = (Player) entity;
-							for(Entity e : Helper.getNearbyEntities(entity.getLocation(), 7)) {
-								if(e instanceof Bat) {
+							for (Entity e : Helper.getNearbyEntities(entity.getLocation(), 7)) {
+								if (e instanceof Bat) {
 									p.playSound(e.getLocation(), Sound.ENTITY_BAT_AMBIENT, 2, 0);
 								}
 							}
-							for(Entity e : Helper.getNearbyEntities(entity.getLocation(), 3)) {
-								if(e instanceof Bat && !e.isDead()) {
-									if(p.getGameMode() == GameMode.ADVENTURE || p.getGameMode() == GameMode.SURVIVAL) {
+							for (Entity e : Helper.getNearbyEntities(entity.getLocation(), 3)) {
+								if (e instanceof Bat && !e.isDead()) {
+									if (p.getGameMode() == GameMode.ADVENTURE || p.getGameMode() == GameMode.SURVIVAL) {
 										double x = e.getLocation().getX();
 										double y = e.getLocation().getY();
 										double z = e.getLocation().getZ();
-										Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "minecraft:summon Creeper "+ x + " " + y + " " + z + " {CustomName:Bat,ExplosionRadius:1b,ignited:1b,Fuse:0s,ActiveEffects:[{Id:1,Amplifier:1,Duration:10,ShowParticles:0b}]}");
-										Advancements.grantAdvancement("gm4/batboozled", p);
+										Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "minecraft:summon creeper " + x + " " + y + " " + z + " {ExplosionRadius:1b,Fuse:0s,Silent:1,NoAI:1,ActiveEffects:[{Id:14,Amplifier:0,Duration:999999}]}");
+										Advancements.grantAdvancement("batboozled", p);
 									}
 								}
 							}
 						}
 					}
 				}
-			}
 		}, 0, 20L);
 	}
 }
