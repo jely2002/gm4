01e15be8aaaf482a0629039267de02c1fe81444e
MaximumFX <draconiconraveleijn@gmail.com>
2019-03-16 10:18:07 +0100

    Changed init to event

--- src/main/java/com/belka/spigot/gm4/modules/BetterArmorStands.java
+++ src/main/java/com/belka/spigot/gm4/modules/BetterArmorStands.java
@@ -2,18 +2,19 @@ package com.belka.spigot.gm4.modules;
 
 import api.Helper;
 import com.belka.spigot.gm4.MainClass;
-import com.belka.spigot.gm4.interfaces.Initializable;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.entity.ArmorStand;
-import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 
-public class BetterArmorStands implements Initializable {
+public class BetterArmorStands implements Listener {
 
 	private MainClass mc;
 
@@ -21,37 +22,34 @@ public class BetterArmorStands implements Initializable {
 		this.mc = mc;
 	}
 
-	public void init(MainClass mc) {
-		mc.getServer().getScheduler().scheduleSyncRepeatingTask(mc, () -> {
-			if(mc.getConfig().getBoolean("modules.BetterArmorStands.enabled")) {
-				for(World world : Bukkit.getWorlds()) {
-					for(Entity entity : world.getEntities()) {
-						if (entity instanceof Item) {
-							ItemStack item = ((Item) entity).getItemStack();
-							if (entity.isOnGround()) {
-								for (Entity e : Helper.getNearbyEntities(entity.getLocation(), 1)) {
-									if (e instanceof ArmorStand) {
-										Material itemMat = ((Item) entity).getItemStack().getType();
-										ArmorStand as = (ArmorStand) e;
-										if (itemMat == Material.STICK && item.getAmount() >= 2 && !as.hasArms()) {
-											as.setArms(true);
-											for (int i = 0; i < 2; i++) {
-												entity.remove();
-											}
-										}
-										if (itemMat == Material.SHEARS && !as.isSmall()) {
-											as.setSmall(true);
-											Damageable itemMeta = (Damageable) item.getItemMeta();
-											itemMeta.setDamage(itemMeta.getDamage() + 1);
-											item.setItemMeta((ItemMeta) itemMeta);
-										}
-									}
-								}
+	@EventHandler
+	public void onItemDrop(PlayerDropItemEvent event) {
+		if (!mc.getConfig().getBoolean("BetterArmorStands.enabled")) return;
+		Item i = event.getItemDrop();
+		ItemStack is = i.getItemStack();
+		final int[] task = new int[]{-1};
+		task[0] = mc.getServer().getScheduler().scheduleSyncRepeatingTask(mc, () -> {
+			if (i.isOnGround()) {
+				Bukkit.getScheduler().cancelTask(task[0]);
+				for (Entity e : Helper.getNearbyEntities(i.getLocation(), 1)) {
+					if (e instanceof ArmorStand) {
+						Material itemMat = i.getItemStack().getType();
+						ArmorStand as = (ArmorStand) e;
+						if (itemMat == Material.STICK && is.getAmount() >= 2 && !as.hasArms()) {
+							as.setArms(true);
+							for (int x = 0; x < 2; x++) {
+								e.remove();
 							}
+						}
+						if (itemMat == Material.SHEARS && !as.isSmall()) {
+							as.setSmall(true);
+							Damageable itemMeta = (Damageable) is.getItemMeta();
+							itemMeta.setDamage(itemMeta.getDamage() + 1);
+							is.setItemMeta((ItemMeta) itemMeta);
 						}
 					}
 				}
 			}
-		}, 0, 20L); // Seconds * 20L
+		}, 0, 10L);
 	}
 }
