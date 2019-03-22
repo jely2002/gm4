be97444e4b4956cf2edceef923a8064373124c90
MaximumFX <draconiconraveleijn@gmail.com>
2019-03-23 00:11:03 +0100

    Small fixes

--- src/main/java/api/Helper.java
+++ src/main/java/api/Helper.java
@@ -42,7 +42,7 @@ public class Helper {
     }
 
     public static List<Block> getNearbyBlocks(Location location, int radius) {
-        List<Block> blocks = new ArrayList<Block>();
+        List<Block> blocks = new ArrayList<>();
         for(int x = location.getBlockX() - radius; x <= location.getBlockX() + radius; x++) {
             for(int y = location.getBlockY() - radius; y <= location.getBlockY() + radius; y++) {
                 for(int z = location.getBlockZ() - radius; z <= location.getBlockZ() + radius; z++) {
