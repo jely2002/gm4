67d88f72d6e70a47d038eed2a9b72483ae3b6248
MaximumFX <draconiconraveleijn@gmail.com>
2019-03-25 21:53:27 +0100

    Working on HeartCanisters

--- src/main/java/com/belka/spigot/gm4/modules/HeartCanisters.java
+++ src/main/java/com/belka/spigot/gm4/modules/HeartCanisters.java
@@ -1,52 +1,102 @@
 package com.belka.spigot.gm4.modules;
 
-import api.SkullCreator;
 import com.belka.spigot.gm4.MainClass;
+import com.belka.spigot.gm4.crafting.CustomItems;
 import com.belka.spigot.gm4.interfaces.Initializable;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
-import java.util.Arrays;
-
-public class HeartCanisters implements Initializable {
+public class HeartCanisters implements Listener, Initializable {
 
 	private MainClass mc;
+	private boolean enabled = true;
 
 	public HeartCanisters(MainClass mc) {
 		this.mc = mc;
 	}
 
 	public void init(MainClass mc) {
-		if(!mc.getConfig().getBoolean("modules.HeartCanisters.enabled")) {
-			return;
+		if(!mc.getConfig().getBoolean("HeartCanisters.enabled")) enabled = false;
+		if(!mc.getConfig().getBoolean("CustomCrafter.enabled")) {
+			System.out.println(ConsoleColor.RED + "Enable CustomCrafter in order for HeartCanisters to work!");
+			enabled = false;
 		}
-		mc.getServer().getScheduler().scheduleSyncRepeatingTask(mc, () -> {
-				for (Entity entity : Bukkit.getWorld("world").getEntities()) {
-					if (entity instanceof Item) {
-						if (entity.isOnGround()) {
-							ItemStack item = ((Item) entity).getItemStack();
-							ItemMeta meta = item.getItemMeta();
-							for (int amount = 1; amount < item.getAmount(); amount++) {
-								ItemStack c = SkullCreator.itemFromUrl("http://textures.minecraft.net/texture/53cf06d5b47b746d16ee631187cb857d8c16bfcd570a40f286b2c38867cfda");
-								c.setAmount(amount);
-								ItemMeta cM = c.getItemMeta();
-								cM.setDisplayName(ChatColor.WHITE + "Heart Cansister");
-								cM.setLore(Arrays.asList(ChatColor.DARK_PURPLE + "Tier 1"));
-								c.setItemMeta(meta);
-								if (item.equals(c)) {
-									Bukkit.broadcastMessage("dsa");
-									meta.setDisplayName(ChatColor.WHITE + "Heart Cansister");
-									meta.setLore(Arrays.asList(ChatColor.DARK_PURPLE + "Tier 1"));
-									item.setItemMeta(meta);
-								}
-							}
-						}
-					}
-				}
-		}, 0, 20L); // Seconds * 20L
+	}
+
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		updateCanisters(e.getPlayer());
+	}
+	@EventHandler
+	public void onClick(InventoryClickEvent e) {
+		if (isCanister(e.getCurrentItem()) && e.getInventory().getHolder() instanceof Player) {
+			Player p = (Player) e.getInventory().getHolder();
+			updateCanisters(p);
+		}
+	}
+	@EventHandler
+	public void onDrag(InventoryDragEvent e) {
+		if (isCanister(e.getNewItems().get(0)) && e.getInventory().getHolder() instanceof Player) {
+			Player p = (Player) e.getInventory().getHolder();
+			updateCanisters(p);
+		}
+	}
+
+	private void updateCanisters(Player p) {
+		if (!enabled) return;
+		boolean hasCansiter = false;
+		int levels = 0;
+		Inventory inv = p.getInventory();
+		for (ItemStack item : inv.getContents()) {
+			if (item == null) continue;
+			if (isCanisterT1(item)) {
+				hasCansiter = true;
+				Bukkit.broadcastMessage("HC1");
+				int amount = item.getAmount();
+				if (amount >= 5) amount = 5;
+				levels = amount * 2;
+			}
+			else if (isCanisterT2(item)) {
+				hasCansiter = true;
+				Bukkit.broadcastMessage("HC2");
+				int amount = item.getAmount();
+				if (amount >= 5) amount = 5;
+				levels = amount * 2;
+			}
+		}
+		if (hasCansiter) {
+			Bukkit.broadcastMessage(levels + "");
+			int level = levels/10;
+			p.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 99999, level, true, false));
+		}
+		else p.removePotionEffect(PotionEffectType.HEALTH_BOOST);
+	}
+
+	private boolean isCanister(ItemStack item) {
+		if (item == null) return false;
+		ItemStack is = item.clone();
+		is.setAmount(1);
+		return (is.equals(CustomItems.HEART_CANISTER_TIER_1(1)) || is.equals(CustomItems.HEART_CANISTER_TIER_2(1)));
+	}
+	private boolean isCanisterT1(ItemStack item) {
+		if (item == null) return false;
+		ItemStack is = item.clone();
+		is.setAmount(1);
+		return (is.equals(CustomItems.HEART_CANISTER_TIER_1(1)));
+	}
+	private boolean isCanisterT2(ItemStack item) {
+		if (item == null) return false;
+		ItemStack is = item.clone();
+		is.setAmount(1);
+		return (is.equals(CustomItems.HEART_CANISTER_TIER_2(1)));
 	}
 }
