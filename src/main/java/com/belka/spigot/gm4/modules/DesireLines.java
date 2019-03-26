da957983a67d629e3b1e5ace24d5a045e3153e5d
maximumfx <Efteling342>
2019-03-26 15:46:13 +0100

    Working on DesireLines

--- src/main/java/com/belka/spigot/gm4/modules/DesireLines.java
+++ src/main/java/com/belka/spigot/gm4/modules/DesireLines.java
@@ -1,6 +1,8 @@
 package com.belka.spigot.gm4.modules;
 
 import com.belka.spigot.gm4.MainClass;
+import com.belka.spigot.gm4.crafting.CustomItems;
+import com.belka.spigot.gm4.interfaces.Initializable;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -12,48 +14,50 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.Random;
 
-public class DesireLines implements Listener {
+public class DesireLines implements Listener, Initializable {
 
 	private MainClass mc;
+	private Random random = new Random();
+	private int max = 50;
+	private int amount = 2;
 
 	public DesireLines(MainClass mc) {
 		this.mc = mc;
 	}
 
-	private Random random = new Random();
+	public void init(MainClass mc) {
+		max = mc.getConfig().getInt("DesireLines.max");
+		amount = mc.getConfig().getInt("DesireLines.amount");
+	}
 
 	@EventHandler
 	public void onMove(PlayerMoveEvent e) {
-		if(mc.getConfig().getBoolean("DesireLines.enabled")) {
-			int max = mc.getConfig().getInt("DesireLines.max");
-			int amount = mc.getConfig().getInt("DesireLines.amount");
-			final Player p = e.getPlayer();
-			if(e.getFrom().getBlock() == e.getTo().getBlock()) {
-				return;
-			}
-			if(random.nextInt(max) <= amount) {
-				Block below = p.getLocation().getBlock();
-				if(below != null && (p.getGameMode() == GameMode.SURVIVAL ||  p.getGameMode() == GameMode.ADVENTURE) && !p.isSneaking()) {
-					Material replace = below.getType();
-					if(replace == Material.AIR) {
-						below = p.getLocation().subtract(0.0, 1.0, 0.0).getBlock();
-					}
-					if(plant(replace)) {
-						Material drop = replace;
-						if (drop == Material.TALL_GRASS) drop = Material.GRASS;
-						else if (drop == Material.LARGE_FERN) drop = Material.FERN;
-						below.getWorld().dropItemNaturally(below.getLocation().add(0.5, 0.5, 0.5), new ItemStack(drop, 1));
-					}
+		if(!mc.getConfig().getBoolean("DesireLines.enabled")) return;
+		if(e.getFrom().getBlock() == e.getTo().getBlock()) return;
 
-					replace = replacement(below.getType());
-					below.setType(replace);
-					below.getState().update();
+		Player p = e.getPlayer();
+		if(random.nextInt(max) <= amount) {
+			Block below = p.getLocation().getBlock();
+			if(below != null && !p.isSneaking() && !p.getInventory().getBoots().equals(CustomItems.BOOTS_OF_OSTARA(1)) &&
+					(p.getGameMode() == GameMode.SURVIVAL ||  p.getGameMode() == GameMode.ADVENTURE)) {
+				Material replace = below.getType();
+				if(replace == Material.AIR) {
+					below = p.getLocation().subtract(0.0, 1.0, 0.0).getBlock();
 				}
+				else if(isPlant(replace)) {
+					Material drop = replace;
+					if (drop == Material.TALL_GRASS) drop = Material.GRASS;
+					else if (drop == Material.LARGE_FERN) drop = Material.FERN;
+					below.getWorld().dropItemNaturally(below.getLocation().add(0.5, 0.5, 0.5), new ItemStack(drop, 1));
+				}
+				replace = getReplacement(below.getType());
+				below.setType(replace);
+				below.getState().update();
 			}
 		}
 	}
 	
-	private Material replacement(Material i) {
+	private Material getReplacement(Material i) {
 		switch (i) {
 			case GRASS_BLOCK:
 				return Material.DIRT;
@@ -62,14 +66,14 @@ public class DesireLines implements Listener {
 			case SAND:
 				return Material.SANDSTONE;
 			default:
-				if(plant(i)) {
+				if(isPlant(i)) {
 					return Material.AIR;
 				}
 				return i;
 		}
 	}
 
-	private boolean plant(Material i) {
+	private boolean isPlant(Material i) {
 		return (i == Material.BROWN_MUSHROOM || i == Material.RED_MUSHROOM || i == Material.DEAD_BUSH || i == Material.GRASS || i == Material.FERN ||
 				i == Material.DANDELION || i == Material.POPPY || i == Material.BLUE_ORCHID || i == Material.ALLIUM || i == Material.AZURE_BLUET || i == Material.OXEYE_DAISY ||
 				i == Material.ORANGE_TULIP || i == Material.PINK_TULIP || i == Material.RED_TULIP || i == Material.WHITE_TULIP ||
