acf0d512cf2e22fbda1dd52aae72765e166928c6
MaximumFX <draconiconraveleijn@gmail.com>
2019-03-26 08:36:02 +0100

    Working on HeartCanisters

--- src/main/java/com/belka/spigot/gm4/modules/HeartCanisters.java
+++ src/main/java/com/belka/spigot/gm4/modules/HeartCanisters.java
@@ -10,7 +10,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -38,14 +37,14 @@ public class HeartCanisters implements Listener, Initializable {
 	}
 	@EventHandler
 	public void onClick(InventoryClickEvent e) {
-		if (isCanister(e.getCurrentItem()) && e.getInventory().getHolder() instanceof Player) {
+		if (e.getInventory().getHolder() instanceof Player) {
 			Player p = (Player) e.getInventory().getHolder();
 			updateCanisters(p);
 		}
 	}
 	@EventHandler
 	public void onDrag(InventoryDragEvent e) {
-		if (isCanister(e.getNewItems().get(0)) && e.getInventory().getHolder() instanceof Player) {
+		if (e.getInventory().getHolder() instanceof Player) {
 			Player p = (Player) e.getInventory().getHolder();
 			updateCanisters(p);
 		}
@@ -53,32 +52,38 @@ public class HeartCanisters implements Listener, Initializable {
 
 	private void updateCanisters(Player p) {
 		if (!enabled) return;
-		boolean hasCansiter = false;
-		int levels = 0;
-		Inventory inv = p.getInventory();
-		for (ItemStack item : inv.getContents()) {
-			if (item == null) continue;
-			if (isCanisterT1(item)) {
-				hasCansiter = true;
-				Bukkit.broadcastMessage("HC1");
-				int amount = item.getAmount();
-				if (amount >= 5) amount = 5;
-				levels = amount * 2;
+		Bukkit.getScheduler().scheduleSyncDelayedTask(mc, new Runnable() {
+			boolean hasCansiter = false;
+			int levels = 0;
+			public void run() {
+				ItemStack[] inv = p.getInventory().getContents();
+
+				ItemStack tier1 = getCanisterT1(inv);
+				if (tier1 != null) {
+					Bukkit.broadcastMessage("HC1");
+					hasCansiter = true;
+					int amount = tier1.getAmount();
+					if (amount >= 5) amount = 5;
+					levels = amount;
+
+					ItemStack tier2 = getCanisterT2(inv);
+					if (tier2 != null) {
+						Bukkit.broadcastMessage("HC2");
+						int amount2 = tier2.getAmount();
+						if (amount2 >= 5) amount2 = 5;
+						levels = levels + amount2;
+					}
+				}
+
+				if (hasCansiter) {
+					Bukkit.broadcastMessage(levels + "");
+					int level = levels - 1;
+					p.removePotionEffect(PotionEffectType.HEALTH_BOOST);
+					p.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 99999, level, true, false));
+				}
+				else p.removePotionEffect(PotionEffectType.HEALTH_BOOST);
 			}
-			else if (isCanisterT2(item)) {
-				hasCansiter = true;
-				Bukkit.broadcastMessage("HC2");
-				int amount = item.getAmount();
-				if (amount >= 5) amount = 5;
-				levels = amount * 2;
-			}
-		}
-		if (hasCansiter) {
-			Bukkit.broadcastMessage(levels + "");
-			int level = levels/10;
-			p.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 99999, level, true, false));
-		}
-		else p.removePotionEffect(PotionEffectType.HEALTH_BOOST);
+		}, 1L);
 	}
 
 	private boolean isCanister(ItemStack item) {
@@ -87,16 +92,27 @@ public class HeartCanisters implements Listener, Initializable {
 		is.setAmount(1);
 		return (is.equals(CustomItems.HEART_CANISTER_TIER_1(1)) || is.equals(CustomItems.HEART_CANISTER_TIER_2(1)));
 	}
-	private boolean isCanisterT1(ItemStack item) {
-		if (item == null) return false;
-		ItemStack is = item.clone();
-		is.setAmount(1);
-		return (is.equals(CustomItems.HEART_CANISTER_TIER_1(1)));
+	private ItemStack getCanisterT1(ItemStack[] inv) {
+		for (ItemStack item : inv) {
+			if (item == null) continue;
+			Bukkit.broadcastMessage(item.toString());
+			ItemStack is = item.clone();
+			is.setAmount(1);
+			if (is.equals(CustomItems.HEART_CANISTER_TIER_1(1))) {
+				return item;
+			}
+		}
+		return null;
 	}
-	private boolean isCanisterT2(ItemStack item) {
-		if (item == null) return false;
-		ItemStack is = item.clone();
-		is.setAmount(1);
-		return (is.equals(CustomItems.HEART_CANISTER_TIER_2(1)));
+	private ItemStack getCanisterT2(ItemStack[] inv) {
+		for (ItemStack item : inv) {
+			if (item == null) continue;
+			ItemStack is = item.clone();
+			is.setAmount(1);
+			if (is.equals(CustomItems.HEART_CANISTER_TIER_2(1))) {
+				return item;
+			}
+		}
+		return null;
 	}
 }
