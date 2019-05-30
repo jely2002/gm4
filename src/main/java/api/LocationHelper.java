7535745d94b6df1a50afc4fee60839a7da1a6f67
jely2002 <jw.glebbeek@gmail.com>
2019-05-30 20:59:06 +0200

    Updated metrics class. Switched to google json.

---
+++ src/main/java/api/LocationHelper.java
@@ -0,0 +1,18 @@
+package api;
+
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class LocationHelper {
+
+    //WIP
+
+    /*public static void writeLoc(Location loc, FileConfiguration config, String path) {
+
+    }
+
+    public static Location readLoc(FileConfiguration config, String path) {
+
+    }*/
+
+}
