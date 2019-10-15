af917d723566ef67d55348b2646703c5b46aed3c
MaximumFX <draconiconraveleijn@gmail.com>
2019-10-15 22:02:46 +0200

    Fixed chunk updating.

--- src/main/java/com/belka/spigot/gm4/customTerrain/CoolerCaves.java
+++ src/main/java/com/belka/spigot/gm4/customTerrain/CoolerCaves.java
@@ -10,231 +10,135 @@ import org.bukkit.block.data.type.Snow;
 import java.util.*;
 
 public class CoolerCaves {
-	/*
-	update per chunk, updated chunk changes bottom bedrock to barrier
 
-	go below player > check chunk by chunk corner
-	go ± X,Y,Z chunk > check chunk
-	for ^ chunks go ± X,Y,Z chunk > check chunk
-	max 10 chunks per direction (render distance)
-	 */
-	private static MainClass mc;
+	private MainClass mc;
 	public CoolerCaves(MainClass mc) {
 		this.mc = mc;
 	}
 
-	private static List<BiomeGroup> updatable = Arrays.asList(BiomeGroup.SNOWY, BiomeGroup.OCEAN, BiomeGroup.DESERT, BiomeGroup.BADLANDS);
-	private static BiomeGroup[] updatableArray = updatable.toArray(new BiomeGroup[0]);
-	private static List<Material> updatableBlocks = Arrays.asList(Material.GRASS, Material.DANDELION, Material.POPPY, Material.ALLIUM, Material.AZURE_BLUET, Material.OXEYE_DAISY, Material.ORANGE_TULIP, Material.PINK_TULIP, Material.RED_TULIP, Material.WHITE_TULIP,
-			Material.STONE, Material.GRASS_BLOCK, Material.DIRT, Material.DIORITE, Material.ANDESITE, Material.SAND, Material.LAVA, Material.OBSIDIAN, Material.OAK_PLANKS, Material.OAK_FENCE, Material.GRAVEL);
+	private List<BiomeGroup> updatable = Arrays.asList(BiomeGroup.SNOWY, BiomeGroup.OCEAN, BiomeGroup.DESERT, BiomeGroup.BADLANDS);
+	private BiomeGroup[] updatableArray = updatable.toArray(new BiomeGroup[0]);
+	private List<Material> updatableBlocks = Arrays.asList(Material.GRASS, Material.DANDELION, Material.POPPY, Material.ALLIUM, Material.AZURE_BLUET, Material.OXEYE_DAISY, Material.ORANGE_TULIP, Material.PINK_TULIP, Material.RED_TULIP, Material.WHITE_TULIP,
+			Material.STONE, Material.GRASS_BLOCK, Material.DIRT, Material.GRANITE, Material.DIORITE, Material.ANDESITE, Material.SAND, Material.LAVA, Material.OBSIDIAN, Material.OAK_PLANKS, Material.OAK_FENCE, Material.GRAVEL);
 
-//	private static RateLimiter limiter = RateLimiter.create(1.0);
-	private static int blocks = 0;
-	private static List<Pair<Map.Entry<Location, Material>, BiomeGroup>> replacements = new ArrayList<>();
-	private static boolean isReplacing = false;
+	private int loadRadius = 3;
+	
+	private List<Pair<Map.Entry<Location, Material>, BiomeGroup>> replacements = new ArrayList<>();
+	private boolean isReplacing = false;
+	private int replacingSpeed = 24576;
 
-//	private static EditSession editSession = WorldEdit.getInstance().getEditSessionFactory().getEditSession(new BukkitWorld(Bukkit.getWorlds().get(0)), -1);
+	private List<Pair<Integer, Integer>> loadedChunks = new ArrayList<>();
 
-	static void loadChunk(Chunk c) {
-		Location corner = new Location(c.getWorld(), c.getX() * 16, 0, c.getZ() * 16);
-		Block nw = corner.getBlock();
-		if (nw.getType() == Material.BEDROCK) {//maybe change to config
-			ChunkSnapshot cs = c.getChunkSnapshot(true, true, false);
-			List<BiomeGroup> nwB = BiomeGroup.getBiomeGroups(cs.getBiome(0, 0), updatableArray);//Get BiomeGroups that biome is in
-			List<BiomeGroup> neB = BiomeGroup.getBiomeGroups(cs.getBiome(15, 0), updatableArray);
-			List<BiomeGroup> seB = BiomeGroup.getBiomeGroups(cs.getBiome(15, 15), updatableArray);
-			List<BiomeGroup> swB = BiomeGroup.getBiomeGroups(cs.getBiome(0, 15), updatableArray);
-			Bukkit.broadcastMessage(nwB.size() + " " + neB.size() + " " + seB.size() + " " + swB.size());
-			if (nwB.size() == 0 && neB.size() == 0 && seB.size() == 0 && swB.size() == 0) return;
+	public void init(MainClass mc) {
+		if (!mc.storage().config().getBoolean("CustomTerrain.enabled") || !mc.storage().config().getBoolean("CustomTerrain.CoolerCaves.enabled")) return;
 
-			if (Helper.allEqual(nwB, neB, seB, swB)) { //If chunk corners have same biome
-				updateFullChunk(c.getWorld(), cs, nwB.get(0));
-			}
-			else { //Chunk corners are unique
-				for (int x = 0 ; x < 16 ; x++ ) {
-					for (int z = 0; z < 16; z++) {
-						List<BiomeGroup> bg = BiomeGroup.getBiomeGroups(cs.getBiome(x, z), updatableArray);
-						if (bg.size() == 0) return;
-						Map<Location, Material> blockMap = new HashMap<>();
-						for(int y = 0; y < cs.getHighestBlockYAt(x, z); y++) {
-							Material mat = cs.getBlockType(x, y, z);
-							if (updatableBlocks.contains(mat))
-								blockMap.put(new Location(c.getWorld(), cs.getX()*16+x, y, cs.getZ()*16+z), mat);
-						}
-						updateBlocks(blockMap, bg.get(0));
-					}
+		replacingSpeed = mc.storage().config().getInt("CustomTerrain.CoolerCaves.replacingSpeed");
+		loadRadius = mc.storage().config().getInt("CustomTerrain.CoolerCaves.loadRadius");
+		if (mc.storage().data().getConfigurationSection("CustomTerrain.CoolerCaves.chunks") == null) return;
+		for (String chunk: mc.storage().data().getStringList("CustomTerrain.CoolerCaves.chunks")) {
+			loadedChunks.add(new Pair<>(Helper.toInteger(chunk.split(" ")[0]), Helper.toInteger(chunk.split(" ")[1])));
+		}
+	}
+
+	public void disable() {
+		List<String> tmp = new ArrayList<>();
+		for (Pair<Integer, Integer> pair: loadedChunks) {
+			tmp.add(pair.getKey() + " " + pair.getValue());
+		}
+		mc.storage().data().set("CustomTerrain.CoolerCaves.chunks", tmp);
+		mc.storage().saveData();
+	}
+
+	void loadChunks(Location loc) {
+		Chunk c = loc.getChunk();
+
+		for (int x = c.getX() - loadRadius; x <= c.getX() + loadRadius; x++) {
+			for (int z = c.getZ() - loadRadius; z <= c.getZ() + loadRadius; z++) {
+				Chunk chunk = c.getWorld().getChunkAt(x, z);
+				if (!loadedChunks.contains(new Pair<>(chunk.getX(), chunk.getZ()))) {
+					loadChunk(chunk);
+					loadedChunks.add(new Pair<>(chunk.getX(), chunk.getZ()));
 				}
 			}
 		}
 	}
 
-	private static void updateFullChunk(World w, ChunkSnapshot cs, BiomeGroup group) {
+	private void loadChunk(Chunk c) {
+		Location corner = new Location(c.getWorld(), c.getX() * 16, 0, c.getZ() * 16);
+		Block nw = corner.getBlock();
+		if (nw.getType() == Material.BARRIER) return;//todo maybe change to config
+
+		ChunkSnapshot cs = c.getChunkSnapshot(true, true, false);
+		List<BiomeGroup> nwB = BiomeGroup.getBiomeGroups(cs.getBiome(0, 0), updatableArray);
+		List<BiomeGroup> neB = BiomeGroup.getBiomeGroups(cs.getBiome(15, 0), updatableArray);
+		List<BiomeGroup> seB = BiomeGroup.getBiomeGroups(cs.getBiome(15, 15), updatableArray);
+		List<BiomeGroup> swB = BiomeGroup.getBiomeGroups(cs.getBiome(0, 15), updatableArray);
+
+		if (nwB.size() == 0 && neB.size() == 0 && seB.size() == 0 && swB.size() == 0) {
+			for (int x = 0 ; x < 16 ; x++ ) {
+				for (int z = 0; z < 16; z++) {
+					Block b = c.getBlock(x, 0, z);
+					b.setType(Material.BARRIER);
+				}
+			}
+			return;
+		}
+
+		if (Helper.allEqual(nwB, neB, seB, swB)) { //If chunk corners have same biome
+			updateFullChunk(c.getWorld(), c, cs, nwB.get(0));
+		}
+		else { //Chunk corners are unique
+			for (int x = 0 ; x < 16 ; x++ ) {
+				for (int z = 0; z < 16; z++) {
+					Block b = c.getBlock(x, 0, z);
+					b.setType(Material.BARRIER);
+
+					List<BiomeGroup> bg = BiomeGroup.getBiomeGroups(cs.getBiome(x, z), updatableArray);
+					if (bg.size() == 0) continue;
+					Map<Location, Material> blockMap = new HashMap<>();
+					for(int y = 0; y < cs.getHighestBlockYAt(x, z) + 1; y++) {
+						Material mat = cs.getBlockType(x, y, z);
+						if (updatableBlocks.contains(mat))
+							blockMap.put(new Location(c.getWorld(), cs.getX()*16+x, y, cs.getZ()*16+z), mat);
+					}
+					updateColumn(blockMap, bg.get(0));
+				}
+			}
+		}
+	}
+
+	private void updateFullChunk(World w, Chunk c, ChunkSnapshot cs, BiomeGroup group) {
 		for (int x = 0 ; x < 16 ; x++ ) {
 			for (int z = 0; z < 16; z++) {
+				Block b = c.getBlock(x, 0, z);
+				b.setType(Material.BARRIER);
+
 				Map<Location, Material> blockMap = new HashMap<>();
-				for(int y = 0; y < cs.getHighestBlockYAt(x, z); y++) {
+				for(int y = 0; y < cs.getHighestBlockYAt(x, z) + 1; y++) {
 					Material mat = cs.getBlockType(x, y, z);
 					if (updatableBlocks.contains(mat))
 						blockMap.put(new Location(w, cs.getX()*16+x, y, cs.getZ()*16+z), mat);
 				}
-				updateBlocks(blockMap, group);
+				updateColumn(blockMap, group);
 			}
 		}
 	}
-	private static void updateBlocks(Map<Location, Material> blockMap, BiomeGroup group) {
-		Bukkit.broadcastMessage("updateBlocks " + group.name());
+
+	private void updateColumn(Map<Location, Material> blockMap, BiomeGroup group) {
 		for (Map.Entry<Location, Material> block : blockMap.entrySet()) {
 			replacements.add(new Pair<>(block, group));
-//			setReplacement(block, group);
 		}
 		updateBlocks();
 	}
 
-//	private static void startReplacing() {
-//		if (isReplacing) return;
-//
-//		int count = 1;
-//		for(int counter = 0; counter < replacements.size(); counter++) {
-//			final int finalCounter = counter;
-//			Bukkit.getScheduler().scheduleSyncDelayedTask(mc, () -> {
-//				Pair<Map.Entry<Location, Material>, BiomeGroup> pair = replacements.get(finalCounter);
-//				setReplacement(pair.getKey(), pair.getValue());
-//			}, count);
-//
-//			if(counter % 3 == 0) {
-//				count++;
-//			}
-//		}
-//		isReplacing = true;
-//	}
-
-	private static void setReplacement(Map.Entry<Location, Material> map, BiomeGroup group) {
-//		limiter.acquire();
-		Block b = map.getKey().getBlock();
-		Material mat = map.getValue();
-		blocks++;
-//		switch (group) {
-//			case SNOWY:
-//				switch (mat) {
-//					case GRASS:
-//						b.setType(Material.SNOW);
-//						((Snow) b.getBlockData()).setLayers(2);
-//					case DANDELION: case POPPY: case BLUE_ORCHID: case ALLIUM: case AZURE_BLUET: case OXEYE_DAISY: case ORANGE_TULIP: case PINK_TULIP: case RED_TULIP: case WHITE_TULIP:
-//						b.setType(Material.SNOW);
-//						((Snow) b.getBlockData()).setLayers(3);
-//					case STONE: case GRASS_BLOCK: b.setType(Material.SNOW_BLOCK);
-//					case DIRT: case GRANITE: b.setType(Material.PACKED_ICE);
-//					case DIORITE: b.setType(Material.BLUE_ICE);
-//					case ANDESITE: b.setType(Material.ICE);
-//					case SAND: b.setType(Material.WHITE_CONCRETE_POWDER);
-//					case LAVA: case OBSIDIAN: b.setType(Material.WATER);
-//				}
-//			case OCEAN:
-//				switch (mat) {
-//					case OAK_PLANKS: b.setType(Material.DARK_PRISMARINE);
-//					case OAK_FENCE: b.setType(Material.PRISMARINE_BRICKS);
-//				}
-//			case DESERT:
-//				switch (mat) {
-//					case STONE: case GRANITE: case DIORITE: case ANDESITE: b.setType(Material.SANDSTONE);
-//					case GRAVEL: b.setType(Material.SAND);
-//					case OAK_PLANKS: b.setType(Material.DARK_OAK_PLANKS);
-//					case OAK_FENCE: b.setType(Material.DARK_OAK_FENCE);
-//				}
-//			case BADLANDS:
-//				switch (mat) {
-//					case STONE: case GRANITE: case DIORITE: case ANDESITE:
-//						int y = map.getKey().getBlockY();
-//						if (y < 12) b.setType(Material.BROWN_TERRACOTTA);
-//						else if (y < 27) b.setType(Material.TERRACOTTA);
-//						else if (y < 29) b.setType(Material.RED_TERRACOTTA);
-//						else if (y < 37) b.setType(Material.ORANGE_TERRACOTTA);
-//						else if (y < 52) b.setType(Material.TERRACOTTA);
-//						else if (y < 58) b.setType(Material.LIGHT_GRAY_TERRACOTTA);
-//						else if (y < 62) b.setType(Material.BROWN_TERRACOTTA);
-//						else b.setType(Material.TERRACOTTA);
-//					case GRAVEL: b.setType(Material.RED_SAND);
-//				}
-//		}
-		switch (group) {
-			case SNOWY:
-				Bukkit.broadcastMessage(blocks + " SNOWY " + mat.name() + " " + group.name());
-				switch (mat) {
-					case GRASS:
-						b.setType(Material.SNOW);
-						((Snow) b.getBlockData()).setLayers(2);
-						break;
-					case DANDELION: case POPPY: case BLUE_ORCHID: case ALLIUM: case AZURE_BLUET: case OXEYE_DAISY: case ORANGE_TULIP: case PINK_TULIP: case RED_TULIP: case WHITE_TULIP:
-						b.setType(Material.SNOW);
-						((Snow) b.getBlockData()).setLayers(3);
-						break;
-					case STONE: case GRASS_BLOCK: changeBlock(b, Material.SNOW_BLOCK); break;
-					case DIRT: case GRANITE: changeBlock(b, Material.PACKED_ICE); break;
-					case DIORITE: changeBlock(b, Material.BLUE_ICE); break;
-					case ANDESITE: changeBlock(b, Material.ICE); break;
-					case SAND: changeBlock(b, Material.WHITE_CONCRETE_POWDER); break;
-					case LAVA: case OBSIDIAN: changeBlock(b, Material.WATER); break;
-				}
-				break;
-			case OCEAN:
-				Bukkit.broadcastMessage(blocks + " OCEAN " + mat.name() + " " + group.name());
-				switch (mat) {
-					case OAK_PLANKS: changeBlock(b, Material.DARK_PRISMARINE); break;
-					case OAK_FENCE: changeBlock(b, Material.PRISMARINE_BRICKS); break;
-				}
-				break;
-			case DESERT:
-				Bukkit.broadcastMessage(blocks + " DESERT " + mat.name() + " " + group.name());
-				switch (mat) {
-					case STONE: case GRANITE: case DIORITE: case ANDESITE: changeBlock(b, Material.SANDSTONE); break;
-					case GRAVEL: changeBlock(b, Material.SAND); break;
-					case OAK_PLANKS: changeBlock(b, Material.DARK_OAK_PLANKS); break;
-					case OAK_FENCE: changeBlock(b, Material.DARK_OAK_FENCE); break;
-				}
-				break;
-			case BADLANDS:
-				Bukkit.broadcastMessage(blocks + " BADLANDS " + mat.name() + " " + group.name());
-				switch (mat) {
-					case STONE: case GRANITE: case DIORITE: case ANDESITE:
-						int y = map.getKey().getBlockY();
-						if (y < 12) changeBlock(b, Material.BROWN_TERRACOTTA);
-						else if (y < 27) changeBlock(b, Material.TERRACOTTA);
-						else if (y < 29) changeBlock(b, Material.RED_TERRACOTTA);
-						else if (y < 37) changeBlock(b, Material.ORANGE_TERRACOTTA);
-						else if (y < 52) changeBlock(b, Material.TERRACOTTA);
-						else if (y < 58) changeBlock(b, Material.LIGHT_GRAY_TERRACOTTA);
-						else if (y < 62) changeBlock(b, Material.BROWN_TERRACOTTA);
-						else changeBlock(b, Material.TERRACOTTA);
-						break;
-					case GRAVEL: changeBlock(b, Material.RED_SAND); break;
-				}
-				break;
-		}
-	}
-	private static void changeBlock(Block b, Material mat) {
-//		try {
-//			editSession.setBlock(BlockVector3.at(b.getX(),b.getY(),b.getZ()), Objects.requireNonNull(BlockTypes.get("minecraft:" + mat.name().toLowerCase())).getDefaultState().toBaseBlock());
-//		} catch (MaxChangedBlocksException ignored) {
-//
-//		}
-
-//		limiter.acquire();
-//		new BukkitRunnable() {
-//			@Override
-//			public void run() {
-				b.setType(mat,false);
-//				Bukkit.broadcastMessage(ChatColor.GOLD + "update i:" + blocks + " mat:" + mat.name() + " x:" + b.getX() + " y:" + b.getY() + " z:" + b.getZ());
-//			}
-//		}.runTask(mc);
-	}
-	private static void updateBlocks() {
+	private void updateBlocks() {
 		if (isReplacing) return;
 		isReplacing = true;
 		final int[] task = new int[]{-1};
 		task[0] = mc.getServer().getScheduler().scheduleSyncRepeatingTask(mc, () -> {
-			int n = 1024;//16384
+//			Bukkit.broadcastMessage("schedule " + replacements.size());
 			List<Pair<Map.Entry<Location, Material>, BiomeGroup>> tmp = new ArrayList<>();
-			for (int i = 0; i < n; i++) {
+			for (int i = 0; i < replacingSpeed; i++) {
 				if (replacements.size() == 0) break;
 				tmp.add(replacements.get(0));
 				replacements.remove(0);
@@ -242,11 +146,76 @@ public class CoolerCaves {
 			for (int i = 0; i < tmp.size(); i++) {
 				Pair<Map.Entry<Location, Material>, BiomeGroup> pair = tmp.get(i);
 				setReplacement(pair.getKey(), pair.getValue());
-				if (tmp.size() < n && i == tmp.size() - 1) {
+				if (tmp.size() < replacingSpeed && i == tmp.size() - 1) {
+//					Bukkit.broadcastMessage("cancel i: " + i + " tmp: " + tmp.size() + " b: " + blocks);
+					Bukkit.broadcastMessage("Updated queued chunks");
 					Bukkit.getScheduler().cancelTask(task[0]);
 					isReplacing = false;
 				}
 			}
 		}, 0L, 20L);
+	}
+
+	private void setReplacement(Map.Entry<Location, Material> map, BiomeGroup group) {
+		Block b = map.getKey().getBlock();
+		Material mat = map.getValue();
+		switch (group) {
+			case SNOWY:
+				switch (mat) {
+					case GRASS:
+						b.setType(Material.SNOW, false);
+						Snow s = (Snow) b.getBlockData();
+						s.setLayers(2);
+						b.setBlockData(s);
+						break;
+					case DANDELION: case POPPY: case BLUE_ORCHID: case ALLIUM: case AZURE_BLUET: case OXEYE_DAISY: case ORANGE_TULIP: case PINK_TULIP: case RED_TULIP: case WHITE_TULIP:
+						b.setType(Material.SNOW, false);
+						Snow s2 = (Snow) b.getBlockData();
+						s2.setLayers(3);
+						b.setBlockData(s2);
+						break;
+					case STONE: case GRASS_BLOCK: b.setType(Material.SNOW_BLOCK, false); break;
+					case DIRT: case GRANITE: b.setType(Material.PACKED_ICE, false); break;
+					case DIORITE: b.setType(Material.BLUE_ICE, false); break;
+					case ANDESITE: b.setType(Material.ICE, false); break;
+					case SAND: b.setType(Material.WHITE_CONCRETE_POWDER, false); break;
+					case LAVA: case OBSIDIAN: b.setType(Material.WATER); break;
+				}
+				break;
+			case OCEAN:
+				switch (mat) {
+					case OAK_PLANKS: b.setType(Material.DARK_PRISMARINE, false); break;
+					case OAK_FENCE: b.setType(Material.PRISMARINE_BRICKS, false); break;
+				}
+				break;
+			case DESERT:
+				switch (mat) {
+					case STONE:
+					case GRANITE:
+					case DIORITE:
+					case ANDESITE:
+						b.setType(Material.SANDSTONE, false); break;
+					case GRAVEL: b.setType(Material.SAND, false); break;
+					case OAK_PLANKS: b.setType(Material.DARK_OAK_PLANKS, false); break;
+					case OAK_FENCE: b.setType(Material.BIRCH_FENCE); break;
+				}
+				break;
+			case BADLANDS:
+				switch (mat) {
+					case STONE: case GRANITE: case DIORITE: case ANDESITE:
+						int y = map.getKey().getBlockY();
+						if (y < 12) b.setType(Material.BROWN_TERRACOTTA, false);
+						else if (y < 27) b.setType(Material.TERRACOTTA, false);
+						else if (y < 29) b.setType(Material.RED_TERRACOTTA, false);
+						else if (y < 37) b.setType(Material.ORANGE_TERRACOTTA, false);
+						else if (y < 52) b.setType(Material.TERRACOTTA, false);
+						else if (y < 58) b.setType(Material.LIGHT_GRAY_TERRACOTTA, false);
+						else if (y < 62) b.setType(Material.BROWN_TERRACOTTA, false);
+						else b.setType(Material.TERRACOTTA, false);
+						break;
+					case GRAVEL: b.setType(Material.RED_SAND, false); break;
+				}
+				break;
+		}
 	}
 }
--- src/main/java/com/belka/spigot/gm4/customTerrain/CustomTerrain.java
+++ src/main/java/com/belka/spigot/gm4/customTerrain/CustomTerrain.java
@@ -2,56 +2,37 @@ package com.belka.spigot.gm4.customTerrain;
 
 import com.belka.spigot.gm4.MainClass;
 import org.bukkit.Bukkit;
-import org.bukkit.Chunk;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Biome;
-import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.world.ChunkLoadEvent;
-
-import java.util.Arrays;
-import java.util.List;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public class CustomTerrain implements Listener {
 
 	private MainClass mc;
-	public CustomTerrain(MainClass mc) {
+	private CoolerCaves coolerCaves;
+	public CustomTerrain(MainClass mc, CoolerCaves coolerCaves) {
 		this.mc = mc;
-	}
-	int i = 0;
-	@EventHandler
-	public void onLoad(ChunkLoadEvent e) {
-		if (!mc.getConfig().getBoolean("CustomTerrain.enabled") || !mc.getConfig().getBoolean("CustomTerrain.CoolerCaves")) return;
-
-		Chunk c = e.getChunk();
-		if (e.getWorld().equals(Bukkit.getWorlds().get(0))) {
-			if (mc.getConfig().getBoolean("CustomTerrain.CoolerCaves")) {
-//				i++;
-//				Bukkit.broadcastMessage(i + ": " + c.getX() + " " + c.getZ());
-//				Bukkit.getScheduler().runTaskAsynchronously(mc, () -> {
-//					CoolerCaves.loadChunk(c);
-//				});
-			}
-		}
+		this.coolerCaves = coolerCaves;
 	}
 
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
-		if (!mc.getConfig().getBoolean("CustomTerrain.enabled") || !mc.getConfig().getBoolean("CustomTerrain.CoolerCaves")) return;
+		if (!mc.getConfig().getBoolean("CustomTerrain.enabled") || !mc.getConfig().getBoolean("CustomTerrain.CoolerCaves.enabled")) return;
 
-		Chunk c = e.getPlayer().getLocation().getChunk();
-		if (e.getPlayer().getWorld().equals(Bukkit.getWorlds().get(0))) {
-			if (mc.getConfig().getBoolean("CustomTerrain.CoolerCaves")) {
-//				i++;
-//				Bukkit.broadcastMessage(i + ": " + c.getX() + " " + c.getZ());
-//				Bukkit.getScheduler().runTaskAsynchronously(mc, () -> {
-				Bukkit.broadcastMessage("Chunk");
-					CoolerCaves.loadChunk(c);
-//				});
-			}
-		}
+		if (mc.getConfig().getBoolean("CustomTerrain.CoolerCaves.enabled"))
+			if (e.getPlayer().getWorld().equals(Bukkit.getWorlds().get(0)))
+				coolerCaves.loadChunks(e.getPlayer().getLocation());
+	}
+
+	@EventHandler
+	public void onPlayerMove(PlayerMoveEvent e) {
+		if (!mc.getConfig().getBoolean("CustomTerrain.enabled") || !mc.getConfig().getBoolean("CustomTerrain.CoolerCaves.enabled")) return;
+
+		if (mc.getConfig().getBoolean("CustomTerrain.CoolerCaves.enabled"))
+			if (e.getPlayer().getWorld().equals(Bukkit.getWorlds().get(0)))
+				if (!e.getFrom().getChunk().equals(e.getTo().getChunk())) {
+					coolerCaves.loadChunks(e.getTo());
+				}
 	}
 }
