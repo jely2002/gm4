d365c656b52615a51f14821ba733ede9822658e9
MaximumFX <draconiconraveleijn@gmail.com>
2019-10-16 15:53:29 +0200

    Removed replace command references.

--- src/main/java/com/belka/spigot/gm4/customTerrain/Ocean.java
+++
@@ -1,8 +0,0 @@
-package com.belka.spigot.gm4.customTerrain;
-
-public class Ocean {
-	/*
-fill ~ 0 ~ ~15 61 ~15 dark_prismarine replace oak_planks
-fill ~ 0 ~ ~15 61 ~15 prismarine_bricks replace oak_fence
-	 */
-}
