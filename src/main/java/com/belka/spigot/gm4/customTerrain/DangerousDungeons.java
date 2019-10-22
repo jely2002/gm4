6553c8e23c7306bb3ee3c774e23539feed8a067e
MaximumFX <draconiconraveleijn@gmail.com>
2019-10-22 19:57:15 +0200

    Added chunk loading with Structure support.

--- src/main/java/com/belka/spigot/gm4/customTerrain/DangerousDungeons.java
+++ src/main/java/com/belka/spigot/gm4/customTerrain/DangerousDungeons.java
@@ -1,13 +1,15 @@
 package com.belka.spigot.gm4.customTerrain;
 
-import api.Helper;
+import api.Structure;
 import com.belka.spigot.gm4.MainClass;
 import org.bukkit.Chunk;
-import org.bukkit.Location;
+import org.bukkit.ChunkSnapshot;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
+import org.bukkit.util.Vector;
 
-import java.util.Objects;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class DangerousDungeons {
 
@@ -18,20 +20,34 @@ public class DangerousDungeons {
 		this.ct = ct;
 	}
 
-	void loadChunk(Chunk c) {
-		Block corner = c.getBlock(0,6,0);
-		if (corner.getType() != Material.GRANITE) return;
+	Structure getStructure(Chunk c) {
+		ChunkSnapshot cs = c.getChunkSnapshot(true, true, false);
+		List<BiomeGroup> biomeGroupList = BiomeGroup.getBiomeGroups(cs.getBiome(8, 8), BiomeGroup.OCEAN, BiomeGroup.DESERT, BiomeGroup.BADLANDS, BiomeGroup.SNOWY);
 
-		Location loc1 = new Location(c.getWorld(), corner.getX()+1, 10, corner.getZ()+1);
-		Location loc2 = new Location(c.getWorld(), corner.getX()-1, 19, corner.getZ()-1);
-
-		for (int i = 0; i < 5; i++) {
-			if (Objects.requireNonNull(Helper.getBlocksBetween(loc1, loc2)).stream().anyMatch(b -> b.getType() == Material.AIR)) {
-				//use Structure https://www.spigotmc.org/threads/structure-block-place-function.344848/
-				return;
-			}
-			loc1.add(0, 10, 0);
-			loc2.add(0, 10, 0);
+		Structure structure = null;
+		if (biomeGroupList.contains(BiomeGroup.OCEAN)) {
+			if (c.getBlock(8, 3, 8).getType() == Material.BEDROCK) structure = getStructure("ocean", new Vector(-8, -7, -8));
+		}
+		else if (biomeGroupList.contains(BiomeGroup.DESERT)) structure = getStructure("desert", new Vector(-8, -7, -8));
+		else if (biomeGroupList.contains(BiomeGroup.BADLANDS)) structure = getStructure("badlands", new Vector(-8, -7, -8));
+		else if (biomeGroupList.contains(BiomeGroup.SNOWY)) structure = getStructure("snowy", new Vector(-8, -7, -8));
+		else {
+			if (c.getBlock(8, 4, 8).getType() == Material.BEDROCK) structure = getStructure("default2", new Vector(-8, -7, -5));
+			else structure = getStructure("default", new Vector(-8, -7, -8));
+		}
+		return structure;
+	}
+	
+	private Structure getStructure(String name, Vector offset) {
+		try {
+			File file = mc.getResourceAsFile("custom_terrain/dangerous_dungeons/" + name + ".nbt");
+			Structure structure = new Structure(file, name);
+			structure.setOffset(offset);
+			return structure;
+		}
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+			return null;
 		}
 	}
 }
