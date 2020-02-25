5ba835908f583065ac971bdc884e15491f56911f
MaximumFX <draconiconraveleijn@gmail.com>
2020-02-25 23:16:59 +0100

    Changed rolls and bonusRolls types

--- src/main/java/api/LootTables/Pool.java
+++ src/main/java/api/LootTables/Pool.java
@@ -3,39 +3,42 @@ package api.LootTables;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.configuration.serialization.SerializableAs;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @SerializableAs("Pool")
 public class Pool implements ConfigurationSerializable {
 	//TODO conditions
-	private int[] rolls;
-	private int[] bonusRolls;
-	private List<Entry> entries;
+	private List<Integer> rolls;//Required
+	private List<Integer> bonusRolls;
+	private List<Entry> entries;//Required
 
-	public Pool(int[] rolls, List<Entry> entries) {
+	public Pool(List<Integer> rolls, List<Entry> entries) {
 		this.rolls = rolls;
 		this.entries = entries;
 	}
+	public Pool(int[] rolls, List<Entry> entries) {
+		this.rolls = Arrays.stream(rolls).boxed().collect(Collectors.toList());;
+		this.entries = entries;
+	}
 	public Pool(Map<String, Object> map) {
-		this.rolls = (int[]) map.get("rolls");
-		if (map.get("bonus_rolls") != null) this.bonusRolls = (int[]) map.get("bonus_rolls");
+		if (map.get("rolls") instanceof Integer) this.rolls = Collections.singletonList((int) map.get("rolls"));
+		else this.rolls = (List<Integer>) map.get("rolls");
+		if (map.get("bonus_rolls") != null) this.bonusRolls = (List<Integer>) map.get("bonus_rolls");
 		this.entries = (List<Entry>) map.get("entries");
 	}
 
-	public int[] getRolls() {
+	public List<Integer> getRolls() {
 		return rolls;
 	}
-	public void setRolls(int[] rolls) {
+	public void setRolls(List<Integer> rolls) {
 		this.rolls = rolls;
 	}
 
-	public int[] getBonusRolls() {
+	public List<Integer> getBonusRolls() {
 		return bonusRolls;
 	}
-	public void setBonusRolls(int[] bonusRolls) {
+	public void setBonusRolls(List<Integer> bonusRolls) {
 		this.bonusRolls = bonusRolls;
 	}
 
@@ -54,12 +57,11 @@ public class Pool implements ConfigurationSerializable {
 		Map<String, Object> map = new HashMap<>();
 
 		map.put("rolls", rolls);
-		map.put("bonus_rolls", bonusRolls);
+		if (bonusRolls != null) map.put("bonus_rolls", bonusRolls);
 		map.put("entries", entries);
 
 		return map;
 	}
-
 	public static Pool deserialize(Map<String, Object> map) {
 		return new Pool(map);
 	}
