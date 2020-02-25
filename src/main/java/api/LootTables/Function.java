5ebbf5a45dd71427c101807d102f177a404dbea5
MaximumFX <draconiconraveleijn@gmail.com>
2020-02-25 23:18:34 +0100

    Added "set_count", "enchant_with_levels", and "levels" functions

--- src/main/java/api/LootTables/Function.java
+++ src/main/java/api/LootTables/Function.java
@@ -2,19 +2,25 @@ package api.LootTables;
 
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.configuration.serialization.SerializableAs;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @SerializableAs("Function")
-public class Function implements ConfigurationSerializable {
-	private String function;
-	private Map<String, Object> tags;
+public class Function implements ConfigurationSerializable {//TODO add other functions
+	private String function;//Required
+	private Map<String, Object> tags;//Required
 
 	public Function(Map<String, Object> map) {
 		this.function = (String) map.get("function");
-		this.tags = (Map<String, Object>) map.get("tags");
+		map.remove("function");
+		this.tags = map;
+	}
+	public Function(String function, Map<String, Object> map) {
+		this.function = function;
+		this.tags = map;
 	}
 
 	public String getFunction() {
@@ -35,7 +41,42 @@ public class Function implements ConfigurationSerializable {
 	public Map<String, Object> serialize() {
 		Map<String, Object> map = new HashMap<>();
 		map.put("function", function);
-		map.put("tags", tags);
+		map.putAll(tags);
 		return map;
+	}
+	public static Function deserialize(Map<String, Object> map) {
+		return new Function(map);
+	}
+
+	public ItemStack apply(ItemStack item) {
+		if (function.contains("set_count")) {
+			if (tags.containsKey("count")) {
+				if (tags.get("count") instanceof List) {
+					List<Integer> count = (List<Integer>) tags.get("count");
+					if (count.size() == 1)
+						item.setAmount(count.get(0));
+					else if (count.size() == 2)
+						item.setAmount(LootTable.getRandomInRange(count.get(0), count.get(1)));
+				}
+			}
+		}
+		else if (function.contains("enchant_with_levels")) {
+			boolean treasure = false;
+			if (tags.containsKey("treasure")) {
+				treasure = (boolean) tags.get("treasure");
+			}
+			if (tags.containsKey("levels")) {
+				if (tags.get("levels") instanceof List) {
+					List<Integer> levels = (List<Integer>) tags.get("levels");
+					int level = 0;
+					if (levels.size() == 1)
+						level = levels.get(0);
+					else if (levels.size() == 2)
+						level = LootTable.getRandomInRange(levels.get(0), levels.get(1));
+					item = Enchantments.enchant(item, level, treasure);
+				}
+			}
+		}
+		return item;
 	}
 }
