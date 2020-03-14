362b93888f19a3047df23be7991e54f22d75f4a1
jely2002 <jw.glebbeek@gmail.com>
2020-03-14 16:39:46 +0100

    fix: BetterFire working with non flaming arrows

--- src/main/java/com/belka/spigot/gm4/modules/BetterFire.java
+++ src/main/java/com/belka/spigot/gm4/modules/BetterFire.java
@@ -2,11 +2,7 @@ package com.belka.spigot.gm4.modules;
 
 import com.belka.spigot.gm4.MainClass;
 import com.belka.spigot.gm4.interfaces.Initializable;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Creeper;
@@ -20,14 +16,12 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.util.BlockIterator;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
 
 public class BetterFire implements Listener, Initializable {
-	//Based of cadenzatb's Better Fire
 	private MainClass mc;
 
 	public BetterFire(MainClass mc) {
@@ -35,71 +29,58 @@ public class BetterFire implements Listener, Initializable {
 	}
 
 	private ArrayList<Arrow> fireArrows = new ArrayList<>();
-	private HashMap<UUID, Integer> checkArrow = new HashMap<>();
+	private HashMap<UUID, Integer> checkArrows = new HashMap<>();
 	private ArrayList<Arrow> deleteArrows = new ArrayList<>();
 
+
 	public void init(MainClass mc) {
-		if(!mc.getConfig().getBoolean("BetterFire.enabled")) return;
+		if(!mc.getStorage().config().getBoolean("BetterFire.enabled")) return;
 		mc.getServer().getScheduler().scheduleSyncRepeatingTask(mc, () -> {
 			for (Arrow arrow : fireArrows) {
-				if (checkArrow.containsKey(arrow.getUniqueId())) {
-					if (checkArrow.get(arrow.getUniqueId()) == arrow.getTicksLived()) {
-						deleteArrows.add(arrow);
-						checkArrow.remove(arrow.getUniqueId());
+				if(arrow.getFireTicks() > 0) {
+					if (checkArrows.containsKey(arrow.getUniqueId())) {
+						if (checkArrows.get(arrow.getUniqueId()) == arrow.getTicksLived()) {
+							deleteArrows.add(arrow);
+							checkArrows.remove(arrow.getUniqueId());
+						} else {
+							checkArrows.put(arrow.getUniqueId(), arrow.getTicksLived());
+						}
 					} else {
-						checkArrow.put(arrow.getUniqueId(), arrow.getTicksLived());
+						checkArrows.put(arrow.getUniqueId(), arrow.getTicksLived());
 					}
+					setFire(arrow);
 				} else {
-					checkArrow.put(arrow.getUniqueId(), arrow.getTicksLived());
-				}
-				Location loc = arrow.getLocation();
-				if(loc.getBlock().getType() == Material.AIR) {
-					arrow.getLocation().getBlock().setType(Material.FIRE);
+					deleteArrows.add(arrow);
 				}
 			}
 			if(deleteArrows.size() > 0) {
-				for(Arrow arrow : deleteArrows) {
+				for (Arrow arrow : deleteArrows) {
 					fireArrows.remove(arrow);
 				}
 			}
-
-		}, 0, 10L);
+		}, 0, 5L);
 	}
 
 	@EventHandler
 	public void onProjectileHit(ProjectileHitEvent e) {
-		if(!mc.getConfig().getBoolean("BetterFire.enabled")) return;
+		if(!mc.getStorage().config().getBoolean("BetterFire.enabled")) return;
+		if(e.getHitEntity() != null) return;
 		if (e.getEntity() instanceof Arrow) {
 			final Arrow arrow = (Arrow)e.getEntity();
-			if (arrow.getFireTicks() == 0) {
-				return;
+			if(!fireArrows.contains(arrow)) {
+				fireArrows.add(arrow);
 			}
-			World world = arrow.getWorld();
-			BlockIterator iterator = new BlockIterator(world, arrow.getLocation().toVector(), arrow.getVelocity().normalize(), 0.0, 4);
-			Block hitBlock;
-			while (iterator.hasNext()) {
-				hitBlock = iterator.next();
-				if (hitBlock.getType() != Material.AIR) break;
+			if (arrow.getFireTicks() > 0) {
+				setFire(arrow);
 			}
-			Bukkit.getScheduler().scheduleSyncDelayedTask(mc, () -> {
-				Location loc = arrow.getLocation();
-				if (arrow.getFireTicks() == 0) {
-					return;
-				}
-				if (loc.getBlock().getType() == Material.AIR) {
-					arrow.getLocation().getBlock().setType(Material.FIRE);
-					fireArrows.add(arrow);
-				}
-			}, 10L);
 		}
 	}
 
 	@EventHandler
 	public void onCreeperDamageByPlayer(EntityDamageByEntityEvent e) {
-		if(!mc.getConfig().getBoolean("BetterFire.enabled")) return;
+		if(!mc.getStorage().config().getBoolean("BetterFire.enabled")) return;
 		if(e.getEntity() instanceof Creeper) {
 			Creeper creeper = (Creeper) e.getEntity();
-			System.out.println(creeper.getHealth());
 			if (creeper.getHealth() <= 2) {
 				if (e.getDamager() instanceof Projectile) {
 					Projectile projectile = (Projectile) e.getDamager();
@@ -126,14 +107,19 @@ public class BetterFire implements Listener, Initializable {
 
 	@EventHandler
 	public void onCreeperTakeDamage(EntityDamageEvent e) {
-		if(!mc.getConfig().getBoolean("BetterFire.enabled")) return;
+		if(!mc.getStorage().config().getBoolean("BetterFire.enabled")) return;
 		if(e.getEntity() instanceof Creeper && (e.getCause() == DamageCause.FIRE_TICK || e.getCause() == DamageCause.FIRE)) {
 			Creeper creeper = (Creeper)e.getEntity();
-			System.out.println(creeper.getHealth());
 			if(creeper.getHealth() <= 2) {
 				creeper.getWorld().createExplosion(creeper.getLocation(), 2.5f, true);
 				creeper.setHealth(0.0);
 			}
+		}
+	}
+
+	private void setFire(Arrow a) {
+		if(a.getWorld().getBlockAt(a.getLocation()).getType() == Material.AIR) {
+			a.getWorld().getBlockAt(a.getLocation()).setType(Material.FIRE);
 		}
 	}
 }
