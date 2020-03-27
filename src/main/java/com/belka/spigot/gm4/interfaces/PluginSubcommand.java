c0e4d3823069dd87e520b1cc12c229d204e50fd4
jely2002 <jw.glebbeek@gmail.com>
2020-03-27 02:46:44 +0100

    Removed deprecated interfaces

--- src/main/java/com/belka/spigot/gm4/interfaces/PluginSubcommand.java
+++
@@ -1,8 +0,0 @@
-package com.belka.spigot.gm4.interfaces;
-
-import org.bukkit.command.CommandSender;
-
-public interface PluginSubcommand {
-    String[] getSubcommand();
-    Boolean onSubcommand(String[] args, CommandSender sender);
-}
